908807c9e1dc3c03b7c9605fbdc802131fdb650d
dimitri-edel <dimitri.edel@gmx.net>
2025-05-17 15:03:38 +0200

    Add panel with a button for saving a conversation inside assistant.html template

--- notes/models.py
+++ notes/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 from assistant.models import Interaction
 
 class Note(models.Model):
+    """
+    The class incapsulates a saved conversation between the user and the assistant.
+    It contains the title of the conversation, the user who created it, and the start and end interactions.
+    """
+
     title = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     interaction_start_id = models.ForeignKey(Interaction, related_name='start_interaction', on_delete=models.CASCADE)
