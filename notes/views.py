560d633d2670923645cec989840e4775936c606e
dimitri-edel <dimitri.edel@gmx.net>
2025-05-17 16:24:06 +0200

    Display notes as a list in notes.html

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Note
 
 # Render the notes app's main page
 def notes_view(request):
     """Renders the notes app's main page."""
-    return render(request, 'notes/notes.html')
+    # Get the user's notes
+    user_notes = Note.objects.filter(user=request.user).order_by('-created_at')
+
+    return render(request, 'notes/notes.html', {'notes': user_notes})
