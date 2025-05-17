79f418e0aca174f353241ecf4137a2960e95a7c2
dimitri-edel <dimitri.edel@gmx.net>
2025-05-17 18:30:26 +0200

    Note views and templates

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import notes_view
+from .views import view_note
 
 urlpatterns = [
     path('', notes_view, name='notes'),  # Render the notes app's main page
+    path('view/<int:note_id>/', view_note, name='view_note'),  # Retrieve the conversation history in the selected note
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Note
+from assistant.models import Interaction
 
 # Render the notes app's main page
 def notes_view(request):
@@ -8,3 +9,18 @@ def notes_view(request):
     user_notes = Note.objects.filter(user=request.user).order_by('-created_at')
 
     return render(request, 'notes/notes.html', {'notes': user_notes})
+
+# Retrive the conversation history in the selected note
+def view_note(request, note_id):
+    """Retrieves the conversation history in the selected note."""
+    # Get the note object
+    note = Note.objects.get(id=note_id)
+
+    # Get the list of interactions in the note
+    interactions = Interaction.objects.filter(
+        usr=request.user,
+        id__gte=note.interaction_start_id.id,
+        id__lte=note.interaction_end_id.id
+    ).order_by('timestamp')
+
+    return render(request, 'notes/view_note.html', {'note': note, 'interactions': interactions})
