516f088cb4f39637a80e6c94002da4944fa3e6e8
dimitri-edel <dimitri.edel@gmx.net>
2025-05-17 13:20:04 +0200

    Create model for notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from assistant.models import Interaction
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interaction_start_id = models.ForeignKey(Interaction, related_name='start_interaction', on_delete=models.CASCADE)
+    interaction_end_id = models.ForeignKey(Interaction, related_name='end_interaction', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
